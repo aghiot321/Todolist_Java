70a365550822146523de8328c8dc5de19d4e3a50
Thiago Barroso da Silva <tbsgudinho@outlook.com>
2025-04-05 20:21:17 -0300

    first commit 1

---
+++ src/main/java/com/aghiot/todolist/user/UserController.java
@@ -0,0 +1,38 @@
+package com.aghiot.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificadores
+ * Public 
+ * Private 
+ * Protected 
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    /**
+     * String (texto)
+     * int (numero inteiro)
+     * boolean (true ou false)
+     * double (duplo)
+     * float (real)
+     * char (caracter)
+     * Date (data)
+     * void (sem retorno)
+     */
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+
+
+    }
+
+
+}
---
+++ src/main/java/com/aghiot/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package com.aghiot.todolist.user;
+
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+    public String email;
+}
