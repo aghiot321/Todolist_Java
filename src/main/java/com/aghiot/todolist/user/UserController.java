d479ec83ab7e2a615ea2ae89c240ed3a4eff527f
Thiago Barroso da Silva <tbsgudinho@outlook.com>
2025-04-09 09:27:00 -0300

    Validação Username

--- src/main/java/com/aghiot/todolist/user/UserController.java
+++ src/main/java/com/aghiot/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.aghiot.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    /**
-     * String (texto)
-     * int (numero inteiro)
-     * boolean (true ou false)
-     * double (duplo)
-     * float (real)
-     * char (caracter)
-     * Date (data)
-     * void (sem retorno)
-     */
-
+    @Autowired
+    private IUserRepository userRepository;
+   
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            throw new RuntimeException("User already exists!");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+
 
 
     }
